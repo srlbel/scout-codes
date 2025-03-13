a7e9903a51e90465e9ddf06bb11bba8ff0544d72
Juan Simancas <dev.juansimancas@proton.me>
2025-03-13 15:18:51 -0500

    initial commit

---
+++ src/murcielago.rs
@@ -0,0 +1,89 @@
+/// The `murcielago` module provides functionality to decode a message
+/// where numbers are substituted for letters based on the "MURCIELAGO" cipher.
+
+/// Represents a coded message that can be decoded.
+pub struct Murcielago {
+    coded_message: String,
+}
+
+impl Murcielago {
+    /// Creates a new `Murcielago` instance.
+    pub fn new(coded_message: &str) -> Self {
+        Self {
+            coded_message: coded_message.to_string(),
+        }
+    }
+
+    /// Decodes the message, replacing numbers with their corresponding letters using 0 as it's base.
+    pub fn decode_message_base0(&self) -> String {
+        let lowercase_code = self.coded_message.to_lowercase();
+
+        let decoded = lowercase_code
+            .chars()
+            .map(|c| match c {
+                '0' => 'm',
+                '1' => 'u',
+                '2' => 'r',
+                '3' => 'c',
+                '4' => 'i',
+                '5' => 'e',
+                '6' => 'l',
+                '7' => 'a',
+                '8' => 'g',
+                '9' => 'o',
+                _ => c,
+            })
+            .collect();
+
+        decoded
+    }
+
+    /// Decodes the message, replacing numbers with their corresponding letters using 1 as it's base.
+    pub fn decode_message_base1(&self) -> String {
+        let lowercase_code = self.coded_message.to_lowercase();
+
+        let decoded = lowercase_code
+            .chars()
+            .map(|c| match c {
+                '1' => 'm',
+                '2' => 'u',
+                '3' => 'r',
+                '4' => 'c',
+                '5' => 'i',
+                '6' => 'e',
+                '7' => 'l',
+                '8' => 'a',
+                '9' => 'g',
+                '0' => 'o',
+                _ => c,
+            })
+            .collect();
+
+        decoded
+    }
+}
+
+/// Decodes a given string using the `Murcielago` cipher base 0 without requiring an instance.
+pub fn decode_base0(coded_message: &str) -> String {
+    Murcielago::new(coded_message).decode_message_base0()
+}
+
+/// Decodes a given string using the `Murcielago` cipher base 1 without requiring an instance.
+pub fn decode_base1(coded_message: &str) -> String {
+    Murcielago::new(coded_message).decode_message_base1()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn should_return_scouts_base0() {
+        assert_eq!(decode_base0("S391TS"), "scouts");
+    }
+
+    #[test]
+    fn should_return_scouts_base1() {
+        assert_eq!(decode_base1("S402TS"), "scouts");
+    }
+}
